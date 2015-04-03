3cc1112161ef124bfbf5b696cce30e5ffbaea362
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-04-02 21:35:36 -0400

    one small step for roomba one giant leap for my PhD :p

--- selene/views.py
+++ selene/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import create
+import time
 
 # Create your views here.
 @api_view(['GET'])
@@ -10,7 +11,7 @@ def walk(request):
   if request.method == 'GET':
     r = create.Create('/dev/tty.ElementSerial-ElementSe')
     r.go(10)
-    sleep(1)
+    time.sleep(1)
     r.stop()
   return
 @api_view(['GET'])

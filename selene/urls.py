48eaee362d95f4d72d67913448f3e452ab22e44e
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-04-03 11:35:00 -0400

    views changes in urls

--- selene/urls.py
+++ selene/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^walk/','selene.views.walk',name='walk'),
-    url(r'^walk/(?P<pk>[0-9]+)$','walk_that'),
-    url(r'^turn_left/','turn_left'),
-    url(r'^turn_right/','turn_right'),
-    url(r'^u_turn/','u_turn'),
-    url(r'^turn/(?P<pk>[0-9]+)$','turn'),
+    url(r'^walk/(?P<pk>[0-9]+)$','selene.views.walk_that',name='walk_that'),
+    url(r'^turn_left/','selene.views.turn_left',name='turn_left'),
+    url(r'^turn_right/','selene.views.turn_right',name='turn_right'),
+    url(r'^u_turn/','selene.views.u_turn',name='u_turn'),
+    url(r'^turn/(?P<pk>[0-9]+)$',,name='turn'),
 ]
